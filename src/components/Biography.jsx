93969bdf0f736cc15813eb6f2157a44c3dec1587
nikolay <n_yozov@hotmail.com>
2022-07-02 02:20:37 -0400

    bio update

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -1,14 +1,11 @@
 import { motion } from "framer-motion";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-function Biography({title}) {
-
+function Biography({ title }) {
   useEffect(() => {
+    document.title = title;
+  }, [title]);
 
-    document.title = title
-   
-  }, [title])
-  
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -17,8 +14,8 @@ function Biography({title}) {
       className="w-full h-screen flex flex-col items-center  dark:bg-black"
     >
       <h1 className="text-3xl font-bold mt-24 dark:text-gray-200">Biography</h1>
-      <div className='w-3/4 sm:w-1/2 mt-5'>
-        <p className='text-sm font-light text-gray-600 dark:text-gray-300'>
+      <div className="w-3/4 sm:w-1/2 mt-5">
+        <p className="text-sm font-light text-gray-600 dark:text-gray-300">
           {" "}
           Hi I'm Nick 👋, a detail-oriented web developer with over a year of
           experience and a passion for creating, optimizing, and improving web
@@ -26,6 +23,16 @@ function Biography({title}) {
           bootcamp graduate ready to take the next step and work with industry
           professionals.
         </p>
+        <br />
+        <p className="text-sm font-light text-gray-600 dark:text-gray-300">
+          I code pretty much everyday, whether it is coding challenges on <a className="text-blue-400" target="_blank" href="https://www.codewars.com/users/nyozov">
+             Codewars
+          </a>
+          , frontend on  <a className="text-blue-400" target="_blank" href="https://www.frontendmentor.io/profile/nyozov">
+             FrontedMentor
+          </a>
+          , or creating the projects that you see here or on my github.
+          </p>
       </div>
     </motion.div>
   );
